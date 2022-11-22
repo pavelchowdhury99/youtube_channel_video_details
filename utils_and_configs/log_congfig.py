f0803d1af0e57cb67e9693cb8a60179b10711ce9
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2022-11-22 10:59:12 +0530

    Add files via upload

--- utils_and_configs/log_congfig.py
+++ utils_and_configs/log_congfig.py
@@ -4,5 +4,5 @@ from utils_and_configs.config import LOG_FILE_LOCATION
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
-                    handlers=[logging.FileHandler(LOG_FILE_LOCATION, mode='a'),
+                    handlers=[logging.FileHandler(LOG_FILE_LOCATION, mode='w+'),
                               logging.StreamHandler()])
